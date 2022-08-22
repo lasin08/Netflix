d9fa1143218db389cd3d721173a4372185429a91
lasin08 <107937958+lasin08@users.noreply.github.com>
2022-08-22 21:37:11 +0530

    Update constants.js

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,5 +1,5 @@
 export const baseUrl = "https://api.themoviedb.org/3/";
-export const API_KEY = "";
+export var API_KEY = process.env.API_KEY;;
  export const imgUrl = "https://image.tmdb.org/t/p/original";
 
 
